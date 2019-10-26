e6b8af29f102653edf63022c77964b81a913c8fd
Caio Moysés de Lima <caio.moyses.lima@gmail.com>
2019-10-26 12:02:39 -0300

    Sincronizando com master

---
+++ APS/src/aps/LogItem.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aps;
+
+/**
+ *
+ * @author cmlima
+ */
+public class LogItem {
+
+//    private static int elapsedTime = -1;
+//    private String PID;
+//    private String[] queue;
+//
+//    public LogItem(String PID, String[] queue) {
+//        this.elapsedTime++;
+//        this.PID = PID;
+//        this.queue = queue;
+//    }
+//
+//    public static int getElapsedTime() {
+//        return elapsedTime;
+//    }
+//
+//    public String getPID() {
+//        return PID;
+//    }
+//
+//    public String[] getQueue() {
+//        return queue;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "LogItem{" + "executingPID=" + executingPID + ", queue=" + queue + '}';
+//    }
+    
+    
+}
---
+++ APS/src/aps/RoundRobin.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aps;
+
+/**
+ *
+ * @author cmlima
+ */
+public class RoundRobin {
+    
+    private int quantum;
+
+    public RoundRobin(int quantum) {
+        this.quantum = quantum;
+    }
+    
+    
+    
+}
