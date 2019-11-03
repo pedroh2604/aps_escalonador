424fe2cff7b8705cb7fc12514689574c677981c6
Caio Moysés de Lima <caio.moyses.lima@gmail.com>
2019-11-02 23:06:21 -0300

    Corrigidos bugs no RandomGenerator

--- APS/src/helpers/RandomGenerator.java
+++ APS/src/helpers/RandomGenerator.java
@@ -3,10 +3,36 @@ package helpers;
 
 import aps.PCB;
 import data_structures.List;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
 
 public class RandomGenerator {
     
+     private static boolean isUnique(int[] array, int num) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int[] toUniqueArray(int[] array) {
+        int[] temp = new int[array.length];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = -1;
+        }
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (isUnique(temp, array[i]))
+                temp[counter++] = array[i];
+        }
+        int[] uniqueArray = new int[counter];
+        System.arraycopy(temp, 0, uniqueArray, 0, uniqueArray.length);
+        return uniqueArray;
+    }
+   
     public static int generateInteger(int min, int max) {
         Random rand = new Random();
         return rand.nextInt(max - min + 1) + min;
@@ -17,7 +43,12 @@ public class RandomGenerator {
             throw new Error("Parâmetros inválidos");
         }
         int arrival = generateInteger(minTime, maxTime - 1);
-        int duration = generateInteger(minTime, maxTime - arrival - 1);
+        int duration;
+        if (maxTime - arrival - 1 <= minTime) {
+            duration = maxTime - minTime;
+        } else {
+            duration = generateInteger(minTime, maxTime - arrival - 1);
+        }
         int requests = generateInteger(-2, 5);
         int ioRequests[];
         if (requests <= 0) {
@@ -26,10 +57,12 @@ public class RandomGenerator {
             if (requests > (duration / 2)) {
                 requests = duration / 2;
             }
-            ioRequests = new int[requests];
+            int temp[] = new int[requests];
             for (int i = 0; i < requests; i++) {
-                ioRequests[i] = generateInteger(arrival, arrival + duration);
+                temp[i] = generateInteger(arrival, arrival + duration);
             }
+            ioRequests = toUniqueArray(temp);
+            Arrays.sort(ioRequests);
         }
         int priority = generateInteger(0, 4);
         return new PCB(arrival, duration, ioRequests, priority);
